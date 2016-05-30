d9f1c6c54e46f4acd5800093c65d9ea7fa836d61
Dan Zeman <zeman@ufal.mff.cuni.cz>
2016-05-30 21:51:14 +0200

    Download the complete examples as well.

--- weaver/download_from_weaver.pl
+++ weaver/download_from_weaver.pl
@@ -31,4 +31,23 @@ foreach my $language (@languages)
     system("perl brat2conllu.pl $language.txt $language.ann > $language.conllu");
     system("git add $language.conllu");
 }
+# The pre-CICLING annotations are here and they should be better quality because they were done by people in the core UD group.
+# http://weaver.nlplab.org/ud/#/complete-examples/
+$baseurl = 'http://weaver.nlplab.org/ud/ajax.cgi?action=downloadFile&protocol=1&collection=%2Fcomplete-examples%2F';
+@languages = qw(cs en ga hu);
+foreach my $language (@languages)
+{
+    print STDERR ("$language complete\n") unless($quiet);
+    foreach my $extension ('txt', 'ann')
+    {
+        my $document = get($baseurl."&document=$language&extension=$extension");
+        my $filename = "$language-complete.$extension";
+        open(FILE, ">$filename") or die("Cannot write $filename: $!");
+        print FILE ($document);
+        close(FILE);
+        system("git add $filename");
+    }
+    system("perl brat2conllu.pl $language-complete.txt $language-complete.ann > $language-complete.conllu");
+    system("git add $language-complete.conllu");
+}
 system("git commit -m 'Downloaded annotation from weaver.nlplab.org.'");
