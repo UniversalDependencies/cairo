eab326c082b3fa2b3b3c5e53e4bda55dc7e34d85
Dan Zeman <zeman@ufal.mff.cuni.cz>
2020-09-20 10:06:36 +0200

    Fixed script.

--- wcsh.sh
+++ wcsh.sh
@@ -1,1 +1,2 @@
-wcc shopen-examples.conllu ; ( wcc shopen-examples.conllu | perl -pe 'm/(\d+) sentences, (\d+) tokens/; $_ = $2/$1." average tokens per sentence\n";' )
+#!/bin/bash
+wc_conll.pl shopen-examples.conllu ; ( wc_conll.pl shopen-examples.conllu | perl -pe 'm/(\d+) sentences, (\d+) tokens/; $_ = $2/$1." average tokens per sentence\n";' )
